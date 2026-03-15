bff6afa340bb506a00c829e0e1e698c68280cbd4
movludahmedov <movlud.ahmedov@gmail.com>
2026-03-15 03:53:36 -0700

    Fix Swift files path

---
+++ PervinMuellim/ViewController.swift
@@ -0,0 +1,39 @@
+﻿import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let config = WKWebViewConfiguration()
+        config.allowsInlineMediaPlayback = true
+        config.mediaTypesRequiringUserActionForPlayback = []
+        config.preferences.javaScriptEnabled = true
+        
+        let dataStore = WKWebsiteDataStore.default()
+        config.websiteDataStore = dataStore
+        
+        webView = WKWebView(frame: view.bounds, configuration: config)
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webView.scrollView.bounces = false
+        view.addSubview(webView)
+        
+        let url = URL(string: "https://pervinmuellim.az/")!
+        webView.load(URLRequest(url: url))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        let html = "<html><body><h2>nternet bağlantısı yoxdur</h2></body></html>"
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+}
