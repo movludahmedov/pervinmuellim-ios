5ed73190baac73f591afc072445b51c6ae2bc090
movludahmedov <movlud.ahmedov@gmail.com>
2026-04-29 06:43:58 -0700

    Update AppDelegate.swift

--- PervinMuellim/AppDelegate.swift
+++ PervinMuellim/AppDelegate.swift
@@ -2,15 +2,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-    
+
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: 
-                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .white
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
+    }
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
